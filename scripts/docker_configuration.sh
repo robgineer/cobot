79fd9c3455fa50182f4ab39a665b0eb24b1f3c9a
robgineer <robgineer@gmail.com>
2025-04-26 15:15:42 +0200

    cleanup scripts for container creation

--- scripts/docker_configuration.sh
+++ scripts/docker_configuration.sh
@@ -1,28 +1,23 @@
 #! /bin/bash
 
-echo " ▗▄▄▖ ▗▄▖ ▗▄▄▖  ▗▄▖▗▄▄▄"
-echo "▐▌   ▐▌ ▐▌▐▌ ▐▌▐▌ ▐▌ █"
-echo "▐▌   ▐▌ ▐▌▐▛▀▚▖▐▌ ▐▌ █"
-echo "▝▚▄▄▖▝▚▄▞▘▐▙▄▞▘▝▚▄▞▘ █"
-echo ""
+echo "  ▗▄▄▖ ▗▄▖ ▗▖  ▗▖▗▄▄▄▖▗▄▖ ▗▄▄▄▖▗▖  ▗▖▗▄▄▄▖▗▄▄▖     ▗▄▄▖ ▗▖ ▗▖▗▄▄▄▖▗▖   ▗▄▄▄  "
+echo " ▐▌   ▐▌ ▐▌▐▛▚▖▐▌  █ ▐▌ ▐▌  █  ▐▛▚▖▐▌▐▌   ▐▌ ▐▌    ▐▌ ▐▌▐▌ ▐▌  █  ▐▌   ▐▌  █ "
+echo " ▐▌   ▐▌ ▐▌▐▌ ▝▜▌  █ ▐▛▀▜▌  █  ▐▌ ▝▜▌▐▛▀▀▘▐▛▀▚▖    ▐▛▀▚▖▐▌ ▐▌  █  ▐▌   ▐▌  █ "
+echo " ▝▚▄▄▖▝▚▄▞▘▐▌  ▐▌  █ ▐▌ ▐▌▗▄█▄▖▐▌  ▐▌▐▙▄▄▖▐▌ ▐▌    ▐▙▄▞▘▝▚▄▞▘▗▄█▄▖▐▙▄▄▖▐▙▄▄▀ "
 
-echo "#################################################################"
-echo "############### Docker Ubuntu 24.04 configuration ###############"
-echo "#################################################################"
-echo ""
 
 if [ ! -e "Dockerfile" ]; then
     echo -e "\nDockerfile not found. Execute this script from a directory with a Dockerfile!"
     exit
 fi
 
-# create the Ubuntu 24.04 image with required configuration (refer Dockerfile for more information)
+# create the Ubuntu 22.04 image with required configuration (refer Dockerfile for more information)
 # and add current user to docker container
-echo "============== Build cobot container =============="
-docker build --build-arg USERNAME=${USER} --build-arg USER_ID=${UID} --build-arg GROUP_ID=$(id -r -g $UID) -t cobot_noble_image .
+echo "============== Build container =============="
+docker build --build-arg USERNAME=${USER} --build-arg USER_ID=${UID} --build-arg GROUP_ID=$(id -r -g $UID) -t artbot_image .
 echo ""
 
-echo "============== Start cobot container =============="
+echo "============== Start container =============="
 # Create instance of built image 
 # bind local home directory to docker container (careful: you have write access to your host home directory),
 # run as current user (with correspinding user id and group id) 
@@ -40,11 +35,11 @@ docker run -itd \
             --network host \
             --rm \
             --entrypoint /bin/bash \
-            --name cobot_noble_container cobot_noble_image
+            --name artbot_container artbot_image
 echo ""
 
 echo "============== Start local docker repo =============="
-docker run -d -p 5000:5000 --name cobot_docker_repo registry:2.7
+docker run -d -p 5000:5000 --name local_docker_repo registry:2.7
 echo ""
 
 echo "============== List current containers =============="
@@ -55,4 +50,4 @@ echo ""
 # TODO @rharbach: commit image in local registry
 
 echo "============== Connect to container =============="
-docker exec -it cobot_noble_container /bin/bash
+docker exec -it artbot_container /bin/bash
