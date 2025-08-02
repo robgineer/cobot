5e9934b0ece0590df6a29131676ef3ea5df395e4
Thao Dang <thao.dang@hs-esslingen.de>
2025-08-02 07:15:33 +0000

    initial calibration gui

---
+++ src/offline_hand_eye/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'offline_hand_eye'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='ubuntu',
+    maintainer_email='thao.dang@hs-esslingen.de',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'record_calib_data = offline_hand_eye.record_calib_data:main'
+        ],
+    },
+)
