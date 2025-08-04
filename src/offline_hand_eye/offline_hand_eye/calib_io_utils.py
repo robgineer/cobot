935e487569d0bc141f88bb44166fb7f1924cd279
Thao Dang <thao.dang@hs-esslingen.de>
2025-08-04 11:49:06 +0200

    added calib gui

--- src/offline_hand_eye/offline_hand_eye/calib_io_utils.py
+++ src/offline_hand_eye/offline_hand_eye/calib_io_utils.py
@@ -1,13 +1,16 @@
 import json
 
-def save_calibration(filename, calibration_config, hand_camera_qwxyz, hand_camera_tr):
+def save_calibration(filename, calibration_config, hand_camera_qwxyz, hand_camera_tr, 
+                     selected_samples=None, data_file_path=None):
     config = {
         'calibration_type': calibration_config['calibration_type'],
         'robot_base_frame': calibration_config['robot_base_frame'],
         'robot_effector_frame': calibration_config['robot_effector_frame'],
         'tracking_base_frame': calibration_config['tracking_base_frame'],
         'translation': hand_camera_tr,
-        'rotation_q_wxyz': hand_camera_qwxyz
+        'rotation_q_wxyz': hand_camera_qwxyz,
+        'selected_samples': selected_samples,
+        'data_file_path': data_file_path
     }
     with open(filename, 'w') as f:
         json.dump(config, f, indent=2)
